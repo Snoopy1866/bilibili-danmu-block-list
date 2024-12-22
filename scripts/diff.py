caca02e101f23bcb79a8618026fcabfb49a76238
Firefly <45487685+Snoopy1866@users.noreply.github.com>
2024-12-22 21:19:47 +0800

    fix: diff.py (#2)

--- scripts/diff.py
+++ scripts/diff.py
@@ -36,22 +36,43 @@ def compare(
     return added_rules, removed_rules
 
 
-def render_diff(
+def convert_single_rule_2_str(rule: dict[str, str]) -> str:
+    """处理单条规则
+
+    Args:
+        rule (dict[str, str]): 规则对象
+
+    Returns:
+        str: 单条规则对应的 Markdown 字符串
+    """
+    filter = rule["filter"]
+    filter = filter.replace("|", "\\|")
+
+    if rule["type"] == 0:
+        type = "文本"
+    elif rule["type"] == 1:
+        type = "正则"
+
+    examples = "、".join(rule.get("examples", []))
+
+    return f"|{filter}|{type}|{examples}|\n"
+
+
+def convert_diff_rules_2_markdown(
     added_rules: list[dict[str, str]], removed_rules: list[dict[str, str]]
 ) -> None:
+    """汇总新增和移除的规则，生成 Markdown 文件
+
+    Args:
+        added_rules (list[dict[str, str]]): 新增的规则列表
+        removed_rules (list[dict[str, str]]): 移除的规则列表
+    """
     with open(DIFF_MARKDOWN, "w", encoding="utf-8") as f:
         f.write("## 新增规则\n")
         f.write("|规则|类型|示例|\n")
         f.write("|----|----|----|\n")
         for rule in added_rules:
-            filter = rule["filter"]
-            filter = filter.replace("|", "\\|")
-            if rule["type"] == 0:
-                type = "文本"
-            elif rule["type"] == 1:
-                type = "正则"
-            examples = "、".join(rule["examples"])
-            f.write(f"|{filter}|{type}|{examples}|\n")
+            f.write(convert_single_rule_2_str(rule))
 
         f.write("\n")
 
@@ -59,21 +80,14 @@ def render_diff(
         f.write("|规则|类型|示例|\n")
         f.write("|----|----|----|\n")
         for rule in removed_rules:
-            filter = rule["filter"]
-            filter = filter.replace("|", "\\|")
-            if rule["type"] == 0:
-                type = "文本"
-            elif rule["type"] == 1:
-                type = "正则"
-            examples = "、".join(rule["examples"])
-            f.write(f"|{filter}|{type}|{examples}|\n")
+            f.write(convert_single_rule_2_str(rule))
 
 
 def main() -> None:
     try:
         added_rules, removed_rules = compare(OLD_JSON, NEW_JSON)
         if added_rules or removed_rules:
-            render_diff(added_rules, removed_rules)
+            convert_diff_rules_2_markdown(added_rules, removed_rules)
     except Exception as e:
         print(e)
         exit(1)
