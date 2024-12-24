5ab7e81efea9f49c6ae7a6cc900a58d5fa068bc3
Kun Jinkao <45487685+Snoopy1866@users.noreply.github.com>
2024-12-24 17:01:25 +0800

    feat: check regex match `examples` and dismatch `excludeExamples`

--- scripts/check.py
+++ scripts/check.py
@@ -1,4 +1,5 @@
 import json
+import re
 from collections import Counter
 from jsonschema import validate
 
@@ -56,6 +57,14 @@ def check_rule_of_user(rules: list[Rule]) -> None:
         raise ValueError(
             f"存在屏蔽用户的规则:\n{"\n".join([str(block_user_rule) for block_user_rule in block_user_rules])}"
         )
+    
+def check_regex_match(rules: list[Rule]) -> None:
+
+    for rule in rules:
+        if rule.type == RuleType.REGEX:
+            try:
+                pattern = re.compile(rule.filter)
+
 
 
 def main() -> None:
