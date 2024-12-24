a4d86668ac97ee8816dff2b53d3eb90976f7da9e
Firefly <45487685+Snoopy1866@users.noreply.github.com>
2024-12-24 12:42:32 +0800

    feat: sort `examples` and `excludeExamples` (#33)

--- scripts/fix_sort.py
+++ scripts/fix_sort.py
@@ -2,15 +2,16 @@ import json
 from pypinyin import lazy_pinyin
 
 from const import BILIBILI_BLOCK
+from utils import Rule
 
 
 def sort() -> None:
-    """按照拼音排序规则"""
+    """按照拼音对规则进行排序"""
     with open(BILIBILI_BLOCK, "r", encoding="utf-8") as f:
         data = json.load(f)
 
-    rules: list[dict] = data["rules"]
-    rules.sort(key=lambda rule: lazy_pinyin(rule["filter"]))
+    rules: list[Rule] = [Rule.from_dict(rule) for rule in data["rules"]]
+    rules.sort(key=lambda rule: lazy_pinyin(rule.filter))
 
     with open(BILIBILI_BLOCK, "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=2)
